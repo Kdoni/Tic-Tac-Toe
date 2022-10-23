c2a0449fe91f4090ef1496257e0677f520c7c522
kdoni <chrisvdoni@gmail.com>
2022-10-23 15:38:33 +0300

    winning condition, plus some optimization pending.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,8 +2,12 @@ class Game
   @player_one = ''
   @player_two = ''
 
-  def self.error_check
+  def self.error_check_player_one
     player_one_move
+  end
+
+  def self.error_check_player_two
+    player_two_move
   end
 
   def self.start_game
@@ -27,26 +31,71 @@ class Game
     move_one = gets.chomp
     case move_one.to_i
     when 1
-      @@board_state[0][0] = 'X'
+      if @@board_state[0][0] == '-'
+        @@board_state[0][0] = 'X'
+      else
+        puts 'The input has already been filled, try another one!'
+        return error_check_player_one
+      end
     when 2
-      @@board_state[0][1] = 'X'
+      if @@board_state[0][1] == '-'
+        @@board_state[0][1] = 'X'
+      else
+        puts 'The input has already been filled, try another one!'
+        return error_check_player_one
+      end
     when 3
-      @@board_state[0][2] = 'X'
+      if @@board_state[0][2] == '-'
+        @@board_state[0][2] = 'X'
+      else
+        puts 'The input has already been filled, try another one!'
+        return error_check_player_one
+      end
     when 4
-      @@board_state[1][0] = 'X'
+      if @@board_state[1][0] == '-'
+        @@board_state[1][0] = 'X'
+      else
+        puts 'The input has already been filled, try another one!'
+        return error_check_player_one
+      end
     when 5
-      @@board_state[1][1] = 'X'
+      if @@board_state[1][1] == '-'
+        @@board_state[1][1] = 'X'
+      else
+        puts 'The input has already been filled, try another one!'
+        return error_check_player_one
+      end
     when 6
-      @@board_state[1][2] = 'X'
+      if @@board_state[1][2] == '-'
+        @@board_state[1][2] = 'X'
+      else
+        puts 'The input has already been filled, try another one!'
+        return error_check_player_one
+      end
     when 7
-      @@board_state[2][0] = 'X'
+      if @@board_state[2][0] == '-'
+        @@board_state[2][0] = 'X'
+      else
+        puts 'The input has already been filled, try another one!'
+        return error_check_player_one
+      end
     when 8
-      @@board_state[2][1] = 'X'
+      if @@board_state[2][1] == '-'
+        @@board_state[2][1] = 'X'
+      else
+        puts 'The input has already been filled, try another one!'
+        return error_check_player_one
+      end
     when 9
-      @@board_state[2][2] = 'X'
+      if @@board_state[2][2] == '-'
+        @@board_state[2][2] = 'X'
+      else
+        puts 'The input has already been filled, try another one!'
+        return error_check_player_one
+      end
     else
       puts 'Invalid option...'
-      return error_check
+      return error_check_player_one
     end
     game_state
     player_two_move
@@ -57,26 +106,71 @@ class Game
     move_one = gets.chomp
     case move_one.to_i
     when 1
-      @@board_state[0][0] = 'O'
+      if @@board_state[0][0] == '-'
+        @@board_state[0][0] = 'O'
+      else
+        puts 'The input has already been filled, try another one!'
+        return error_check_player_two
+      end
     when 2
-      @@board_state[0][1] = 'O'
+      if @@board_state[0][1] == '-'
+        @@board_state[0][1] = 'O'
+      else
+        puts 'The input has already been filled, try another one!'
+        return error_check_player_two
+      end
     when 3
-      @@board_state[0][2] = 'O'
+      if @@board_state[0][2] == '-'
+        @@board_state[0][2] = 'O'
+      else
+        puts 'The input has already been filled, try another one!'
+        return error_check_player_two
+      end
     when 4
-      @@board_state[1][0] = 'O'
+      if @@board_state[1][0] == '-'
+        @@board_state[1][0] = 'O'
+      else
+        puts 'The input has already been filled, try another one!'
+        return error_check_player_two
+      end
     when 5
-      @@board_state[1][1] = 'O'
+      if @@board_state[1][1] == '-'
+        @@board_state[1][1] = 'O'
+      else
+        puts 'The input has already been filled, try another one!'
+        return error_check_player_two
+      end
     when 6
-      @@board_state[1][2] = 'O'
+      if @@board_state[1][2] == '-'
+        @@board_state[1][2] = 'O'
+      else
+        puts 'The input has already been filled, try another one!'
+        return error_check_player_two
+      end
     when 7
-      @@board_state[2][0] = 'O'
+      if @@board_state[2][0] == '-'
+        @@board_state[2][0] = 'O'
+      else
+        puts 'The input has already been filled, try another one!'
+        return error_check_player_two
+      end
     when 8
-      @@board_state[2][1] = 'O'
+      if @@board_state[2][1] == '-'
+        @@board_state[2][1] = 'O'
+      else
+        puts 'The input has already been filled, try another one!'
+        return error_check_player_two
+      end
     when 9
-      @@board_state[2][2] = 'O'
+      if @@board_state[2][2] == '-'
+        @@board_state[2][2] = 'O'
+      else
+        puts 'The input has already been filled, try another one!'
+        return error_check_player_two
+      end
     else
       puts 'Invalid option...'
-      return error_check
+      return error_check_player_two
     end
     game_state
     player_one_move
@@ -87,6 +181,8 @@ class Game
     puts "#{@@board_state[1][0]}|#{@@board_state[1][1]}|#{@@board_state[1][2]}"
     puts "#{@@board_state[2][0]}|#{@@board_state[2][1]}|#{@@board_state[2][2]}"
   end
+
+  def self.winning_condition; end
 
   @game_over = false
 
